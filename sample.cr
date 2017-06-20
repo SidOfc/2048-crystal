c5d78dcee50550dbc58ddb2078c6aca3596b6750
Sidney Liebrand <sidneyliebrand@gmail.com>
2017-06-20 23:41:40 +0200

    sample file ignored?

--- sample.cr
+++ sample.cr
@@ -7,7 +7,13 @@ if seq && seq.is_a? Array
   mapping = {"l" => :left, "r" => :right, "u" => :up, "d" => :down}
   seq     = seq.to_s.split(',').map { |c| mapping[c] }
 
-  count.times { puts TwentyFortyEight.sample { seq.find { |dir| move dir } }.score }
+  count.times do
+    result = TwentyFortyEight.sample { seq.find { |dir| move dir } }.score
+    puts result if TwentyFortyEight.options.verbose
+  end
 else
-  count.times { puts TwentyFortyEight.sample.score }
+  count.times do
+    result = TwentyFortyEight.sample.score
+    puts result if TwentyFortyEight.options.verbose
+  end
 end
